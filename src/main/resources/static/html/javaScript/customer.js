fc21f6714776c3976499a177f3bbc087202191f5
AlexandrTairov <Bubaka_210899>
2021-08-26 12:35:24 +0400

    Add ajax

--- src/main/resources/static/html/javaScript/customer.js
+++ src/main/resources/static/html/javaScript/customer.js
@@ -1,3 +1,9 @@
 function customer() {
-    console.log(window.opener.myDatamyData);
+    var element = document.getElementById('customerName');
+
+    element.value = "User1";
+
+
+        // document.getElementById('Name').setAttribute("User1");
+        // document.getElementById("Email").setAttribute("Email1@Test1");
 }
--- src/main/resources/static/html/javaScript/customerService.js
+++ src/main/resources/static/html/javaScript/customerService.js
@@ -1,4 +1,4 @@
-function function1() {
+function function1() { // get by id
     let id;
     if (document.getElementById('customerId1') != null) {
         id = document.getElementById("customerId1").value;
@@ -8,4 +8,46 @@ function function1() {
         .then(response => response.json())
         .then(data => console.log(data))
     let win = window.open("customer.html", "Customer");
+}
+
+function function2() { //getAll
+
+}
+
+function function3() { // create
+    let customerName, customerEmail;
+    if (document.getElementById('customerName1') != null) {
+        customerName = document.getElementById("customerName1").value;
+    }
+    if (document.getElementById('customerEmail1') != null) {
+        customerEmail = document.getElementById("customerEmail1").value;
+    }
+
+    $.ajax({
+        dataType: 'json',
+        contentType: "application/json",
+        url: "http://localhost:8080/customer/create",
+        type: 'POST',
+        data:  JSON.stringify({name: customerName, email: customerEmail}), //if no JSON is available use the one from https://github.com/douglascrockford/JSON-js
+        success: function(data) {
+            console.log("Here ", data);
+            // $("#updateInboxView").html(data);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert(jqXHR + " : " + textStatus + " : " + errorThrown);
+        }
+    });
+    // const url = "http://localhost:8080/customer/create";
+    // fetch(url)
+    //     .then(customerName, customerEmail)
+    //     .then(response => response.json())
+    //     .then(data => console.log(data))
+}
+
+function function4() { //delete
+
+}
+
+function function5() { //update
+
 }
